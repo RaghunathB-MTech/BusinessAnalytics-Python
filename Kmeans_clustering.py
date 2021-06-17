43f4e751337619f663d14acb19ef63d2da272eb7
Raghunath B <75133931+RaghunathB-MTech@users.noreply.github.com>
2021-06-17 16:24:10 +0530

    Add files via upload

---
+++ Kmeans_clustering.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 08:33:14 2021
+
+@author: Raghunath B
+"""
+
+#Project: Customer segmentation
+#K-means clustering
+
+#Load the dataset
+import pandas as pd
+df = pd.read_csv('D:/Analytics/projects/Lecture programs/7. K-means clustering/Mall_Customers.csv')
+
+x = df.iloc[:,[3,4]].values
+
+#Elbow method - To find the optimum no. of clusters
+from sklearn.cluster import KMeans
+wcss = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+import matplotlib.pyplot as plt
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow method')
+plt.xlabel('No. of clusters')
+plt.ylabel('WCSS')
+#Optimum no. of clusters(k)=5 since a steep drop is observed only till i=5
+#There is not much variability in the data for k>5
+
+#Fit K-means to the dataset
+kmeans = KMeans(n_clusters=5, init='k-means++',random_state=0)
+y_kmeans = kmeans.fit_predict(x)
+
+#Visualizing the clusters
+plt.scatter(x[y_kmeans==0,0], x[y_kmeans==0,1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(x[y_kmeans==1,0], x[y_kmeans==1,1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(x[y_kmeans==2,0], x[y_kmeans==2,1], s = 100, c = 'cyan', label = 'Cluster 3')
+plt.scatter(x[y_kmeans==3,0], x[y_kmeans==3,1], s = 100, c = 'green', label = 'Cluster 4')
+plt.scatter(x[y_kmeans==4,0], x[y_kmeans==4,1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+
+#Cluster 1: Medium Income and Spending Score --> Regular customers
+#Cluster 2: Low Income and High SS --> Lavish spenders
+#Cluster 3: High Income and High SS --> Elite buyers
+#CLuster 4: Low Income and SS --> Poor buyers
+#Cluster 5: High Income but Low SS --> People who seek offers
+
+
+
+
+
+
+
+
+
